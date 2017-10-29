ea54e3ea4152fbb25f6e754d89f8a87aa15b6b6b
Marcin Biernat <mb@marcinbiernat.pl>
2017-10-29 21:54:12 +0100

    Display file details in a table

--- src/App/View.purs
+++ src/App/View.purs
@@ -5,14 +5,14 @@ import App.State.FileInputs as FileInputs
 import App.State.Files as Files
 import App.State.Signers as Signers
 import Data.Map (toUnfoldable)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Data.Traversable (for_)
 import Data.Tuple (Tuple(..))
 import Lib.SignHash.Types (HashSigner(..), ProofMethod, ProofVerification(..))
 import Prelude hiding (div,id)
 import Pux.DOM.Events (onChange, onDragOver, onDrop)
 import Pux.DOM.HTML (HTML, child)
-import Text.Smolder.HTML (div, h1, hr, input, label, li, ul)
+import Text.Smolder.HTML (div, h1, hr, input, label, li, table, tbody, td, th, thead, tr, ul)
 import Text.Smolder.HTML.Attributes (className, for, id, type')
 import Text.Smolder.Markup (text, (!), (#!))
 
@@ -43,7 +43,9 @@ view { file, signer } =
 viewFileInput :: Unit -> HTML FileInputs.Event
 viewFileInput _ =
   div do
-    label ! for "file-upload" ! className "file-upload"
+    label
+      ! for "file-upload"
+      ! className "file-upload"
       #! onDrop FileInputs.newFilesEvent
       #! onDragOver (FileInputs.PreventDefault FileInputs.NoOp)
       $ text "Click or drag and drop files"
@@ -57,25 +59,21 @@ viewFileInput _ =
 viewFile :: Files.State -> HTML Event
 viewFile { meta, result, signer } =
   div do
-    div $ text ("Filename: " <> meta.name)
-    div $ text ("Size: " <> show meta.size <> " Bytes")
-    div resultComponent
-  where
-    resultComponent = case result of
-      Nothing ->
-        div $ text $ "Loading..."
-      Just value -> do
-        div $ text $ "sha256: " <> value.hash
-        div $ text $ "elapsed:" <> show value.elapsed
-        signerComponent
+    table ! className "u-full-width" $ tbody $ do
+      renderRow "FileName" meta.name
+      renderRow "Size" $ show meta.size <> " Bytes"
+      renderRow "SHA256" $ maybe loading _.hash result
+      renderRow "Signer address" $ maybe loading renderSigner signer
 
-    signerComponent = case signer of
-      Nothing ->
-        div $ text $ "Fetching signer..."
-      Just (HashSigner address) ->
-        div $ text $ "Signer address: " <> address
-      Just NoSigner ->
-        div $ text $ "Not signed"
+  where
+    loading = "Loading..."
+    renderRow label value =
+      tr $ do
+        th $ text label
+        td $ text value
+
+    renderSigner (HashSigner address) = address
+    renderSigner NoSigner = "Not signed"
 
 
 viewSigner :: Signers.State -> HTML Signers.Event
