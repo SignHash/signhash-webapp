e6c0b417d43507dd843d1f5fc4d9a86799124744
Marcin Biernat <mb@marcinbiernat.pl>
2017-10-30 22:41:53 +0100

    Add FontAwesome

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 require('../styles/normalize.css');
 require('../styles/skeleton.css');
 require('../styles/signhash.css');
+require('../styles/font-awesome.css');
 
 
 exports.load = function () {};
