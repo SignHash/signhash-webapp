eb4ee00856713bb21991346d333b9f87a6f428eb
Marcin Biernat <mb@marcinbiernat.pl>
2017-11-01 08:57:13 +0100

    Nicer file upload prompt
    Shrinking file upload bar

--- src/App/View.purs
+++ src/App/View.purs
@@ -7,11 +7,11 @@ import App.State.FileInputs as FileInputs
 import App.State.Files as Files
 import App.State.Signers as Signers
 import Data.Map (toUnfoldable)
-import Data.Maybe (Maybe(..), maybe)
+import Data.Maybe (Maybe(..), isJust, maybe)
 import Data.Traversable (for_)
 import Data.Tuple (Tuple(..))
 import Lib.SignHash.Types (HashSigner(..), ProofMethod, ProofVerification(..))
-import Prelude (Unit, discard, show, unit, ($), (<>))
+import Prelude (discard, show, ($), (<>))
 import Pux.DOM.Events (onChange, onDragOver, onDrop)
 import Pux.DOM.HTML (HTML, child)
 import Text.Smolder.HTML.Attributes (className, for, id, type')
@@ -24,39 +24,36 @@ view { file, signer } =
     div ! className "content" $ do
       h1 $ text "SignHash"
       hr
-      child FileInput viewFileInput unit
-      hr
+      child FileInput viewFileInput $ isJust file
       case file of
-        Nothing -> text $ ""
+        Nothing -> empty
         Just value -> do
+          viewFile value
           h4 $ text "Signers"
           div signerStatus
-      h4 $ text "File status"
-      div fileStatus
 
   where
-    fileStatus = case file of
-      Nothing ->
-        div $ text "Please provide a file"
-      Just value -> viewFile value
-
     signerStatus = case signer of
       Nothing -> div do
         text loading
         hr
-      Just value -> child Signer viewSigner value
+      Just value -> child Signer viewSigner $ value
 
 
-viewFileInput :: Unit -> HTML FileInputs.Event
-viewFileInput _ =
+viewFileInput :: Boolean -> HTML FileInputs.Event
+viewFileInput small =
   div do
     label
       ! for "file-upload"
-      ! className "file-upload"
+      ! className ("file-upload" <> if small then " small" else "")
       #! onDrop FileInputs.newFilesEvent
       #! onDragOver (FileInputs.PreventDefault FileInputs.NoOp)
-      $ div do
-        h3 $ text "Click or drag and drop files"
+      $ do
+        renderIcon "fa-file-o"
+        text "Verify a file"
+        if small then empty else
+          div ! className "hint"
+          $ text "Click or drag and drop on the page"
 
     input
       ! id "file-upload"
@@ -65,12 +62,11 @@ viewFileInput _ =
 
 
 viewFile :: Files.State -> HTML Event
-viewFile { meta, result, signer } =
-  table ! className "u-full-width" $ tbody $ do
-    renderRow "FileName" meta.name
+viewFile { meta, result, signer } = do
+  table ! className "u-full-width file-table" $ tbody $ do
+    renderRow "Name" meta.name
     renderRow "Size" $ show meta.size <> " Bytes"
     renderRow "SHA256" $ maybe loading _.hash result
-    renderRow "Signer address" $ maybe loading renderSigner signer
 
   where
     renderRow label value =
@@ -110,20 +106,24 @@ viewProofs proofs =
         (Tuple icon msg) = proofDetails state
 
     proofDetails Signers.Pending =
-      Tuple "fa fa-spinner" ""
+      Tuple "fa-spinner" ""
     proofDetails Signers.NetworkError =
-      Tuple "fa fa-exclamation-triangle" "Network error"
+      Tuple "fa-exclamation-triangle" "Network error"
     proofDetails (Signers.Finished (Verified msg)) =
-      Tuple "fa fa-check" msg
+      Tuple "fa-check" msg
     proofDetails (Signers.Finished (Unverified msg)) =
-      Tuple "fa fa-exclamation-circle" $ "Failed: " <> msg
+      Tuple "fa-exclamation-circle" $ "Failed: " <> msg
     proofDetails (Signers.Finished Unavailable) =
-      Tuple "fa fa-ban" "No proof defined"
+      Tuple "fa-ban" "No proof defined"
 
 
 renderIcon :: forall a. String -> HTML a
-renderIcon name = i ! className ("fa-lg " <> name) $ text ""
+renderIcon name = i ! className ("fa fa-lg " <> name) $ text ""
 
 
 loading :: String
 loading = "Loading..."
+
+
+empty :: forall a. HTML a
+empty = text ""
